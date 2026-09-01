155e81b3febc7c8b555cfaf31fb2fafbcf21443a
Jun <jun.cho725@gmail.com>
2026-09-01 10:15:19 +0900

    feat: 리커버리 유형 테스트 페이지 추가, 기기관리 CTA 연결

---
+++ app/recovery-type-test/page.tsx
@@ -0,0 +1,7 @@
+import RecoveryTypeTest from "@/components/recovery-type-test/RecoveryTypeTest";
+
+export const metadata = { title: "리커버리 유형 테스트 | WIM CENTER" };
+
+export default function Page() {
+  return <RecoveryTypeTest />;
+}
